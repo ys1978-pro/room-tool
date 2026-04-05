d8852ff16d1c9afd745c3f0f504878ee05e895db
ys1978-pro <shiotani0720@gmail.com>
2026-04-05 09:53:48 +0900

    Update rakuten.js

--- netlify/functions/rakuten.js
+++ netlify/functions/rakuten.js
@@ -1,16 +1,18 @@
-exports.handler = async (event) => {
-  const keyword = event.queryStringParameters?.keyword || '美容液';
+const fetch = require('node-fetch');
+
+exports.handler = async function(event, context) {
+  const keyword = (event.queryStringParameters && event.queryStringParameters.keyword) || '美容液';
   const APP_ID = '335c3f9c-6e96-4226-9471-4766fec2d117';
   const ACCESS_KEY = 'pk_thp8WuFagFNOQh9VnsoWHJ8mAQhhRsHt4NWvW4wUA4q';
 
-  const url = https://openapi.rakuten.co.jp/ichibams/api/IchibaItem/Search/20220601
-    + ?format=json
-    + &keyword=${encodeURIComponent(keyword)}
-    + &genreId=555086
-    + &applicationId=${APP_ID}
-    + &accessKey=${ACCESS_KEY}
-    + &hits=8
-    + `&sort=-reviewCount`;
+  const url = 'https://openapi.rakuten.co.jp/ichibams/api/IchibaItem/Search/20220601'
+    + '?format=json'
+    + '&keyword=' + encodeURIComponent(keyword)
+    + '&genreId=555086'
+    + '&applicationId=' + APP_ID
+    + '&accessKey=' + ACCESS_KEY
+    + '&hits=8'
+    + '&sort=-reviewCount';
 
   try {
     const res = await fetch(url);
