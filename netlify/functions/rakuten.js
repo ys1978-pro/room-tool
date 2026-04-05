d92a5147eb48da176948544310a572e7dfb54ab5
ys1978-pro <shiotani0720@gmail.com>
2026-04-05 10:09:50 +0900

    Update rakuten.js

--- netlify/functions/rakuten.js
+++ netlify/functions/rakuten.js
@@ -1,31 +1,34 @@
 exports.handler = async function(event, context) {
-  const keyword = (event.queryStringParameters && event.queryStringParameters.keyword) || '美容液';
-  const APP_ID = '335c3f9c-6e96-4226-9471-4766fec2d117';
-  const ACCESS_KEY = 'pk_thp8WuFagFNOQh9VnsoWHJ8mAQhhRsHt4NWvW4wUA4q';
+  var keyword = '美容液';
+  if (event.queryStringParameters && event.queryStringParameters.keyword) {
+    keyword = event.queryStringParameters.keyword;
+  }
 
-  const url = 'https://openapi.rakuten.co.jp/ichibams/api/IchibaItem/Search/20220601'
+  var APP_ID = '335c3f9c-6e96-4226-9471-4766fec2d117';
+  var ACCESS_KEY = 'pk_thp8WuFagFNOQh9VnsoWHJ8mAQhhRsHt4NWvW4wUA4q';
+
+  var url = 'https://openapi.rakuten.co.jp/ichibams/api/IchibaItem/Search/20220601'
     + '?format=json'
     + '&keyword=' + encodeURIComponent(keyword)
     + '&applicationId=' + APP_ID
     + '&accessKey=' + ACCESS_KEY
-    + '&hits=8'
-    + '&sort=-reviewCount';
+    + '&hits=8';
 
   try {
-    const res = await fetch(url);
-    const data = await res.json();
+    var res = await fetch(url);
+    var text = await res.text();
     return {
       statusCode: 200,
       headers: {
         'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
       },
-      body: JSON.stringify(data),
+      body: text
     };
   } catch (e) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: e.message }),
+      body: JSON.stringify({ error: e.message })
     };
   }
 };
